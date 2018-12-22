ff4b63ee1cd2234f6d7aa4f14e9ec71be308c964
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2018-12-22 20:33:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "!";
 var adminprefix = '!'
 
-const developers = ["518484338707529728"]
+const developers = ["521763526436585472"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
