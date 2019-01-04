a5f4e72d59f4d741d41ec23398fa03d1688f3681
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2019-01-04 14:07:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -99,5 +99,22 @@ client.on("message", (message) => {
  
 });
 
+client.on('message', message => {
+    if (message.content.startsWith("^avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
