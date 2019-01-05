b2bd38e9ddf9cb0d08d1dba87ae8f02b15a1653d
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2019-01-05 13:11:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const { Client, Util } = require('discord.js');
-const { TOKEN ,PREFIX, GOOGLE_API_KEY } = require('./config1');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 var prefix = "^";
