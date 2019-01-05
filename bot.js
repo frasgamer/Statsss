c6b95c0b8755aae940b6c09d86f00594563e5c45
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2019-01-05 16:30:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -540,8 +540,6 @@ function play(guild, song) {
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
  
-const adminprefix = "$vip";
-const devs = ['274923685985386496'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
